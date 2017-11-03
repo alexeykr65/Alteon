28b3f3615d45886fa4475db15a388c69ea3aeb06
Alexey Karpov <alex@akarpov.net>
2017-11-03 20:01:25 +0300

    Add Reset Config from Alteon

--- alteon.py
+++ alteon.py
@@ -13,46 +13,79 @@ import requests
 import zlib
 import datetime
 
-ALTEON = ['192.168.180.61', '192.168.180.62', '192.168.180.63', '192.168.180.64']
-USER = "admin"
-PASSWORD = "admin"
-myAuth=requests.auth.HTTPBasicAuth(USER, PASSWORD)
+listAlteon = ['192.168.180.61', '192.168.180.62', '192.168.180.63', '192.168.180.64', '192.168.180.75', '192.168.180.76']
+altUser = "admin"
+altPass = "admin"
+myAuth=requests.auth.HTTPBasicAuth(altUser, altPass)
 
-
-description = "alteon: Get information from Alteon"
+description = "alteon: Get and Set Information from Alteon"
 epilog = "ciscoblog.ru"
 
-listParamConfig = ['internal_interface','internal_lan','external_interface']
 flagDebug = int()
 flagL2vpn = int()
+flagReset = int()
 sterraConfig = dict()
-flagFullMesh = 0
-flagCentral = 1
 
 fileName = ""
 keyPreShare = ""
 nameInterface = ""
 
 def getCfgAlteon():
-    global ALTEON, USER, PASSWORD, myAuth, flagDebug
-    myAuth=requests.auth.HTTPBasicAuth(USER, PASSWORD)
-    for sAlteon in ALTEON:
+    global flagDebug
+    
+    for sAlteon in listAlteon:
         if flagDebug > 1: print "Alteon Ip address: ", sAlteon
-        reqSTR =  "https://"+sAlteon+"/config/getcfg"
-        requests.packages.urllib3.disable_warnings()
-        r = requests.get(reqSTR, auth=myAuth, verify=False)
-        rr = zlib.decompress(r.content, zlib.MAX_WBITS|32)
-        year, month, day, hour, minn, sec = getDate()
-        fileName = '-'.join(["Alteon", sAlteon, "config", year, month, day, hour, minn, sec + ".txt"])
-        if flagDebug > 1: print " Filename cfg: "+fileName
-        writeFile(rr, fileName)
-            
+        reqUrl = "https://"+sAlteon+"/config/getcfg"            
+        r = getRequests(reqUrl, 1, "")
+        if(r):
+            zDecompress = zlib.decompress(r.content, zlib.MAX_WBITS|32)
+            year, month, day, hour, minn, sec = getDate()
+            fileName = '-'.join(["Alteon", sAlteon, "config", year, month, day, hour, minn, sec + ".txt"])
+            if flagDebug > 1: print " Filename cfg: "+fileName
+            writeFile(zDecompress, fileName)
+
+def rstAlteon():
+    global flagDebug, flagReset
+    
+    if(flagReset == 0):
+        for sAlteon in listAlteon:
+            if flagDebug > 1: print "Alteon Ip address: ", sAlteon
+            reqUrl = "https://"+sAlteon+"/config"            
+            r = getRequests(reqUrl, 2, '{"agReset":"2"}')
+    else:
+        sAlteon = listAlteon[flagReset-1]
+        if flagDebug > 1: print "Alteon Ip address: ", sAlteon
+        reqUrl = "https://"+sAlteon+"/config"            
+        r = getRequests(reqUrl, 2, '{"agReset":"2"}')
+    
+def getRequests(pReqUrl, pMethod, pParam):
+    global myAuth, flagDebug    
+    requests.packages.urllib3.disable_warnings()
+    count = 4
+    while(count > 1):
+        try:
+            if(pMethod == 1):
+                r = requests.get(pReqUrl, auth=myAuth, verify=False, timeout=10)
+            elif(pMethod == 2):
+                r = requests.put(pReqUrl, auth=myAuth, verify=False, timeout=10, data=pParam)
+        except requests.ConnectionError, e:
+            print "Error : ", e 
+            return 0
+        if(r.status_code == 200):
+            if flagDebug > 1: print "Status: ", r.status_code
+            count = 0
+        else:
+            count = count - 1
+            if flagDebug > 1: print "Status else: ", r.status_code
+    return r
+          
 def cmdArgsParser():
     global fileName, keyPreShare, nameInterface, flagDebug, flagFullMesh, flagL2vpn
     if flagDebug > 0: print "Analyze options ... "
     parser = argparse.ArgumentParser(description=description, epilog=epilog)
     parser.add_argument('-f', '--file', help='File name with source data', dest="fileName", default = 'sterra.conf')
     parser.add_argument('-c', '--getcfg', help='Get Config From Alteon', action="store_true")
+    parser.add_argument('-r', '--reset', help='Reset Alteons', action="store", dest="flagReset", default=0)
     parser.add_argument('-i', '--interface', help='Name of interfaces (default GigabitEthernet)', action="store",  dest="nameInterface", default="GigabitEthernet")
     parser.add_argument('-d', '--debug', help='Debug information view(default =1, 2- more verbose', dest="flagDebug", default=1)
     parser.add_argument('-m', '--mesh', help='Enable Full Mesh(default = disable', action="store_true")
@@ -61,9 +94,6 @@ def cmdArgsParser():
     return parser.parse_args()
 
 def getDate():
-    '''
-    This function returns a tuple of the year, month and day.
-    '''
     #Get Date
     now = datetime.datetime.now()
     day = str(now.day)
@@ -73,43 +103,42 @@ def getDate():
     minn = str(now.minute)
     sec = str(now.second)
     
-    
-    #Prepend '0' to day and month if only a single digit (better for alpha sorting)
     if len(day) == 1:
         day = '0' + day
     if len(month) == 1:
         month = '0' + month
-
+    if len(hour) == 1:
+        hour = '0' + month
+    if len(minn) == 1:
+        minn = '0' + month
+    if len(sec) == 1:
+        sec = '0' + month
     return year, month, day, hour, minn, sec
 
 def writeFile(raw, filename):
-    '''
-    This function simply write the contents of the "raw" variable to a 
-    file with the name passed to the function.  The file suffix is .txt by
-    default unless a different suffix is passed in.
-    '''
     newfile = open(filename, 'wb')
     newfile.write(raw)
     newfile.close()
 
 def main():
-    global flagDebug
+    global flagDebug, flagReset
     args = cmdArgsParser()
-    print "Begin module Main \n"
+    #flagDebug = int(args.flagDebug)
     
     if(args.getcfg):
-        if flagDebug > 1: print " Module Main, args - getcfg\n"
+        if flagDebug > 1: print "Get Config from Alteon"
         getCfgAlteon()
-     
-    # Kill vm with PIDs
-    # PowerON vm with PIDs
-    
+    if(args.flagReset):
+        flagReset = int(args.flagReset)
+        if flagDebug > 1: print "Flag Reset : ", flagReset
+        if(flagReset == 0):
+            if flagDebug > 1: print "Reset all Alteons"
+        rstAlteon()
+        
     sys.exit()
 
-if __name__ == '__main__':
-    
-#    if secret == "": secret = getpass.getpass('Password:')
-    
-    print "Begin Programm \n"
+if __name__ == '__main__':    
+#    if secret == "": secret = getpass.getpass('Password:')   
+    print "Begin Programm ..."
     sys.exit(main())
 
